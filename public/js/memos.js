5034e1ccb6db27cc213fb51cb1b0f6fb62644a3e
Hazuuuu <fazizie@gmail.com>
2023-07-02 13:05:10 +0800

    update api

--- public/js/memos.js
+++ public/js/memos.js
@@ -6,7 +6,7 @@ console.log(
 photos();
 //查询所有用户api “https://memos.smitten.top/api/memo/all?tag=相册”
 function photos() {
-    fetch("https://memo.hazu.in/api/memo?openId=a3ada300-e7d2-49e5-b51a-b130c0609717")
+    fetch("https://memo.hazu.in/api/memo?creatorId=1&tag=gaming_friend")
     .then((res) => res.json())
     .then((data) => {
       // 记得修改memos地址
